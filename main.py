920eeb8ce3d2642fde126c7a321df96e59504450
Govarthenan Rajadurai <govarthenan@gmail.com>
2024-07-29 17:28:59 +0530

    feat: Add user registration endpoint to API

--- main.py
+++ main.py
@@ -1,8 +1,76 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body, Response, status
 
+import psycopg.rows
+from pydantic import BaseModel
+
+import psycopg
+from psycopg.rows import dict_row
+
+from utils import hash_password
+
+# database configuration
+db_name = "travelpoint"
+user = "postgres"
+root_password = "root"
+DSN = f"dbname={db_name} user={user} password={root_password}"
+
+#  try to connect to database
+conn: psycopg.connection.Connection  # type hint
+try:
+    # type hints for IDE support
+    conn: psycopg.Connection
+    cur: psycopg.Cursor
+
+    conn = psycopg.connect(DSN, row_factory=dict_row) # type: ignore
+    cur = conn.cursor()
+except Exception as e:
+    print(f"Error connecting to DB:\n{e}")
+    exit(1)
+
+# test database
+# read all rows
+# cur.execute("SELECT * FROM user_info")
+# result = cur.fetchall()
+#
+# print("Printing rows")
+# i: psycopg.rows.DictRow
+# for i in result:
+#     print(i)
+
+# pydantic data models
+
+
+class UserRegistration(BaseModel):
+    first_name: str
+    last_name: str
+    phone_number: int
+    location: str
+    password: str
+
+
+# server
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.post("/user_management/register", status_code=status.HTTP_201_CREATED)
+async def register_user(payload: UserRegistration = Body(...), response: Response = Response()):
+    hashed_password = hash_password(payload.password)
+
+    query = b"""INSERT INTO user_info (first_name, last_name, phone_number, location, password) \
+            VALUES (%s, %s, %s, %s, %s)"""
+
+    try:
+        cur.execute(
+            query, (payload.first_name, payload.last_name, payload.phone_number, payload.location, hashed_password)
+        )
+        conn.commit()
+
+        return {"message": "User registered!"}
+    except Exception as e:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return {"detail": str(e)}
